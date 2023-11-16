e1d99f7af0f32f71f276e76c3f017fe1fdadc4fc
WilliamAlmeida1 <139846121+WilliamAlmeida1@users.noreply.github.com>
2023-11-16 13:59:18 -0300

    Update UrnaLiderDeSala.c

--- SistemaDeUrnaParaVotacao/UrnaLiderDeSala.c
+++ SistemaDeUrnaParaVotacao/UrnaLiderDeSala.c
@@ -11,34 +11,34 @@ int main(){
         scanf("%d", &voto);
         switch(voto){
     case 1:
-        candidatos[0]++;
-        break;
-    case 2:
         candidatos[1]++;
         break;
-    case 3:
+    case 2:
         candidatos[2]++;
         break;
-    case 4:
+    case 3:
         candidatos[3]++;
+        break;
+    case 4:
+        candidatos[4]++;
         break;
     default:
         printf("DIGITOU UM NUMERO INVALIDO!! \n\n");
-        candidatos[3]++;
+        candidatos[4]++;
         break;
         }
         contador++;
     }
-    printf("Quantidade de pessoas que votaram: [%d]\n", pessoas);
-    printf("Quantidade de pessoas que votaram no Ailson Salviano: [%d]\n", candidatos[0]);
-    printf("Quantidade de pessoas que votaram na Kamille Paulino: [%d]\n", candidatos[1]);
+    printf("\n---\nQuantidade de pessoas que votaram: [%d]\n", pessoas);
+    printf("Quantidade de pessoas que votaram no Ailson Salviano: [%d]\n", candidatos[1]);
+    printf("Quantidade de pessoas que votaram na Kamille Paulino: [%d]\n", candidatos[3]);
     printf("Quantidade de pessoas que votaram no Danis Daniel: [%d]\n", candidatos[2]);
-    printf("Quantidade de pessoas que votaram NULO ou INVALIDO: [%d]\n", candidatos[3]);
-        if(candidatos[0] > candidatos[1] && candidatos[0] > candidatos[2]){
+    printf("Quantidade de pessoas que votaram NULO ou INVALIDO: [%d]\n", candidatos[4]);
+        if(candidatos[1] > candidatos[2] && candidatos[1] > candidatos[3]){
         printf("Ganhador da votação: Ailson Salviano\n");
-    }else if(candidatos[1] > candidatos[0] && candidatos[1] > candidatos[2]){
+    }else if(candidatos[2] > candidatos[1] && candidatos[2] > candidatos[3]){
         printf("Ganhador da votação: Danis Daniel\n");
-    }else if(candidatos[2] > candidatos[0] && candidatos[2] > candidatos[1]){
+    }else if(candidatos[3] > candidatos[1] && candidatos[3] > candidatos[2]){
         printf("Ganhador da votação: Kamille Paulino\n");
     }else{
         printf("Houve um empate entre candidatos!\n");
