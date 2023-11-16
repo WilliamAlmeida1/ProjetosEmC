8497f8ec91e6f90e384a315a34d3b8934f4a1880
WilliamAlmeida1 <139846121+WilliamAlmeida1@users.noreply.github.com>
2023-11-16 06:16:54 -0300

    Create SistemaBancarioEmC.c

---
+++ SistemaDeCaixaEletronicoComWhile/SistemaBancarioEmC.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    float saldo = 0, tran;//variaveis para saldo e valor da transação
+    int operacao, sair = 0;//variavel que guarda o tipo de transação e a outra que guarda uma condição para continuar rodando
+    printf("Seja bem vindo ao caixa bancário Will!\n\n");
+    while(sair == 0){
+        printf("-OPÇÕES-\n(1) Consultar seu saldo\n(2) Depositar\n(3) Realizar um saque\n(4) Sair do sistema\n\nDigite o numero que corresponde à operação que você deseja realizar: ");
+        scanf("%d", &operacao);
+        switch(operacao){
+        case 1:
+            printf("Seu saldo é: %.2f reais\n\n", saldo);
+            break;
+        case 2:
+            printf("Digite o valor a ser depositado: ");
+            scanf("%f", &tran);
+            saldo = saldo + tran;
+            printf("Depósito concluído! Novo saldo: %.2f\n\n", saldo);
+            break;
+        case 3:
+            printf("Digite o valor a ser sacado: ");
+            scanf("%f", &tran);
+            saldo = saldo - tran;
+            printf("Saque concluído! Novo saldo: %.2f\n\n", saldo);
+            break;
+        case 4:
+            printf("Saindo do sistema!!!\n\n");
+            sair = 1;
+            break;
+        default:
+            printf("Você digitou um número invalido!!!\n\n");
+            break;
+        }
+    }
+
+}
